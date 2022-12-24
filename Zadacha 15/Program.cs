695ebcc75254ea052bc549eab84c5596ed19c657
dmitriy <dima-dim@yandex.ru>
2022-12-24 10:53:14 +0300

    Решение

--- Zadacha 15/Program.cs
+++ Zadacha 15/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+
+string[] week = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресение" };
+
+Console.WriteLine("Введите номер дня недели");
+int num = int.Parse(Console.ReadLine());
+
+if (num > 7)
+{
+    Console.WriteLine("В неделе вроде 7 дней?");
+}
+else if (num > 5)
+{
+    Console.WriteLine($"Сегодня {week[num - 1]}, выходной!");
+}
+else
+{
+    Console.WriteLine($"Сегодня {week[num - 1]}, рабочий день :(");
+}
